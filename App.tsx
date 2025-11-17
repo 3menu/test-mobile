aa6bc465b9ce073aedc6f8b1597d1980216b3363
3menu <thanhlin36@gmail.com>
2025-11-17 10:15:12 +0700

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <main className="h-screen w-screen bg-white overflow-hidden">
+      <iframe
+        src="https://script.google.com/macros/s/AKfycbzC5YfFuHV6z4wCbowUTkf8qC3wJJoFcVX1eBDTrO1gfHLPbOQQaYQnMCDTWVKqtQM/exec"
+        className="w-full h-full border-none"
+        title="Cậu Ốc Menu"
+        loading="lazy"
+      ></iframe>
+    </main>
+  );
+};
+
+export default App;
