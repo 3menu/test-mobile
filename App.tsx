16b176a41eae8bce0ba7f2748a9da21cd566b0af
3menu <thanhlin36@gmail.com>
2025-11-17 10:47:25 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,1 +1,21 @@
 
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <main className="bg-gray-50 h-screen w-screen flex items-center justify-center">
+      <div className="relative w-full max-w-[500px] mx-auto h-screen overflow-hidden border border-gray-300 shadow-lg md:h-[90vh] md:rounded-xl">
+        <iframe
+          id="menu-iframe"
+          src="https://script.google.com/macros/s/AKfycbzC5YfFuHV6z4wCbowUTkf8qC3wJJoFcVX1eBDTrO1gfHLPbOQQaYQnMCDTWVKqtQM/exec"
+          className="w-full h-full border-none"
+          loading="lazy"
+          title="3enu"
+        >
+        </iframe>
+      </div>
+    </main>
+  );
+};
+
+export default App;
