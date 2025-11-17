728c4e56749b7cb9d61fcdf09b17c2e1339600f5
3menu <thanhlin36@gmail.com>
2025-11-17 11:06:44 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ const App: React.FC = () => {
       <div className="relative w-full max-w-[500px] mx-auto h-screen overflow-hidden border border-gray-300 shadow-lg md:h-[90vh] md:rounded-xl">
         <iframe
           id="menu-iframe"
-          src="https://script.google.com/macros/s/AKfycbz6KyEzjpTYAFhe-QAtGa316gq24wFnzqBauYsPI5FlHDeF_A7wP2OGGg25wtNzhfha/exec"
+          src="https://script.google.com/macros/s/AKfycbxu7GQCsUoD2N30xc5bck6wHuUwdcsXw90EI6GuXtE8kgIk3j5GYVFymodly2wtDzuL/exec"
           className="w-full h-full border-none"
           loading="lazy"
           title="3enu"
