47b0a5f565a3267d8f1663abd16b7b88caf5e66a
3menu <thanhlin36@gmail.com>
2025-11-17 10:18:43 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME = 'pwa-wrapper-cache-v1';
-const urlsToCache = [ '/', '/index.html' ]; // Chỉ cần cache trang vỏ bọc
-
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))));
